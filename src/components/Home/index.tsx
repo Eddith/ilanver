668e46c257447edefa28c95de146f3e777b1ee19
Ali Koçman <34556282+Eddith@users.noreply.github.com>
2022-05-05 16:52:56 +0300

    :bug: home, header, footer page added

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 
+// TODO: next imports
+import Image from "next/image";
+
+// TODO: import components
+import Card from "./Card";
+
 const Home: React.FC = () => {
   return (
     <>
-      <h2>Home</h2>
+      <div className="bg-white">
+        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
+            Customers also purchased
+          </h2>
+
+          <Card />
+        </div>
+      </div>
     </>
   );
 };
